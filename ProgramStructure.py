e0f9f0ec13e5cc49d6e39a67e30aba584a46ffef
Chompoo <knwchbr@MacBook-Pro-khxng-knwchbr.local>
2019-12-01 15:41:51 +0700

    txt

--- ProgramStructure.py
+++ ProgramStructure.py
@@ -1,41 +1,40 @@
-def Sentence(file_contents,Sentences):
-    Sentences = file_contents.split('.')
+def Sentence(file_txt,Sentences):
+    Sentences = file_txt.split('.')
     print(len(Sentences) - 1 )
 
-def All_Word(file_contents) :
-    print(len(file_contents.split()))
+def CountWord(file_txt) :
+    print(len(file_txt.split()))
 
-def All_Letter(file_contents,Letter) :
-    for i in file_contents :
+def CountLetter(file_txt,Letter) :
+    for i in file_txt :
         if i.isalpha() == True :
             Letter += 1
         if i == '.' or i == ',' or i == ':' or i == ';' or i == '/' or i == '(' or i == ')' or i == '-'  :
             Letter += 1
     print(Letter)
         
-def All_Line(file_contents,The_Line) :
-    print(len(file_contents.split('\n')))
+def CountLine(file_txt,The_Line) :
+    print(len(file_txt.split('\n')))
 
-def All_Upper(file_contents,Upper) :
-    for i in file_contents :
+def CountUpper(file_txt,Upper) :
+    for i in file_txt :
         if i.isupper() is True :
             Upper += 1
     print(Upper)
 
-def All_Lower(file_contents,Lower) :
-    for i in file_contents :
+def CountLower(file_txt,Lower) :
+    for i in file_txt :
         if i.islower() is True :
             Lower += 1
     print(Lower)
 
-def All_Special(file_contents,Special) :
-    for i in file_contents :
+def CountSpecial(file_txt,Special) :
+    for i in file_txt :
         if i == '.' or i == ',' or i == ':' or i == ';' or i == '/' or i == '(' or i == ')' or i == '-'  :
             Special += 1
     print(Special)
 
 def main() :
-# All Value
     Sentences = 0
     Letter = 0
     The_Line = 0
@@ -43,22 +42,14 @@ def main() :
     Lower = 0
     Special = 0
     infile = open('Loop.txt','r+')
-    file_contents = infile.read()
-    list(file_contents)
-#Count Sentence
-    Sentence(file_contents,Sentences) #Complete
-#Count Word
-    All_Word(file_contents) #Complete
-#Count Letter
-    All_Letter(file_contents,Letter) #Complete
-#Count Line
-    All_Line(file_contents,The_Line) #Complete
-#Count Upper
-    All_Upper(file_contents,Upper) #Complete
-#Count Lower
-    All_Lower(file_contents,Lower) #Complete
-
-#Count Special Letter
-    All_Special(file_contents,Special) #Complete
+    file_txt = infile.read()
+    list(file_txt)
+    Sentence(file_txt,Sentences)
+    CountWord(file_txt)
+    CountLetter(file_txt,Letter)
+    CountLine(file_txt,The_Line)
+    CountUpper(file_txt,Upper)
+    CountLower(file_txt,Lower)
+    CountSpecial(file_txt,Special)
 
 main()
